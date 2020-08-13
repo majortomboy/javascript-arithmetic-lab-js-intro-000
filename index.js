9847c67ba8c685bb0c24381d1ae8f3d50507cd1f
Amy DuPont <amykdupont@gmail.com>
2020-08-13 23:42:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+5 + 2
+console.log(5 + 2)
+6 - 3
+7 * 10
+100 / 10
+subtract(6 , 3)
+multiply(7 , 10)
+divide(100 , 10)
+increment(5)
+decrement(20)
+parseInt('4.3256' , 10)
+parseFloat('2.234' , 10)
