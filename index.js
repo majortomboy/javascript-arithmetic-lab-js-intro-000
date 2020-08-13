de3c4871116ddc27669cab6840aefe16cf59897f
Amy DuPont <amykdupont@gmail.com>
2020-08-13 23:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function add(){}
 function subtract(){}
 function multiply(){}
 function divide(){}
-add (1530 , 7)
-subtract (0 , -59)
-multiply (20 * 9526)
-divide (100 / 10)
-function increment(){}
-function decrement(){}
+function increment(){
+  return ++
+}
+increment(5)
